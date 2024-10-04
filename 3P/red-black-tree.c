ea14e9ef80f5be66fd5deb795dec6de14ba127a7
gabrielbatistabarbosa4 <gabrielbatistabarbosa4@gmail.com>
2024-10-04 13:22:08 +0000

    red black tree fixed file

--- 3P/red-black-tree.c
+++ 3P/red-black-tree.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum Cor {
-    NEGRO,
-    RUBRO
-}
+#define NEGRO 0
+#define RUBRO 1
 
 typedef struct no {
     int valor;
@@ -189,7 +187,17 @@ void criarNo(tipoArvore *arvore, int valor) {
     return;
 }
 
+tipoNo* maximo(tipoNo* no) {
+    printf("Entrando no nó (%d)\n", no->valor);
+    // Encontra o maior valor na subárvore à esquerda
+    while (no->dir != NULL) {
+        no = no->dir;
+    }
+    return no;
+}
+
 tipoNo* minimo(tipoNo* no) {
+    printf("Entrando no nó (%d)\n", no->valor);
     // Encontra o menor valor na subárvore à direita
     while (no->esq != NULL) {
         no = no->esq;
@@ -197,21 +205,61 @@ tipoNo* minimo(tipoNo* no) {
     return no;
 }
 
-void substituirNo(tipoArvore *arvore, tipoNo *no, tipoNo *filho) {
-    if (no->pai == NULL) {
-        // Se o nó removido é a raiz
-        arvore->raiz = filho;
-    } else if (no == no->pai->esq) {
-        no->pai->esq = filho;
-    } else {
-        no->pai->dir = filho;
-    }
-    
-    if (filho != NULL) {
-        filho->pai = no->pai;
-    }
-}
+void substituirNo(tipoArvore *arvore, tipoNo *noVelho, tipoNo *noNovo) {
+    // caso 1: substituir por nulo
+    if (noNovo == NULL) {
+        // se noVelho é raiz
+        if (noVelho->pai == NULL) {
+            arvore->raiz = NULL;
+            printf("A raiz (%d) foi excluída!\n\n", noVelho->valor);
+            return;
+        }
 
+        if (noVelho == noVelho->pai->esq) {
+            // no velho é filho esquerdo do pai
+            noVelho->pai->esq = NULL;
+        } else {
+            // no velho é filho direito do pai
+            noVelho->pai->dir = NULL;
+        }
+        printf("Substituindo (%d) por nulo!\n\n", noVelho->valor);
+        return;
+    }
+
+    // caso 2: no velho é raiz
+    if (noVelho->pai == NULL) {
+        arvore->raiz = noNovo;
+        noNovo->pai = NULL;
+        noNovo->esq = NULL;
+        noNovo->dir = NULL;
+        printf("Substituindo (%d) por (%d)!\n\n", noVelho->valor, noNovo->valor);
+        return;
+    }
+
+    // caso 3: no Velho é filho esquerdo do pai
+    if (noVelho == noVelho->pai->esq) {
+        noVelho->pai->esq = noNovo;
+    }
+    // caso 4: no velho é filho direito do pai
+    else if (noVelho == noVelho->pai->dir) {
+        noVelho->pai->dir = noNovo;
+    }
+
+    noNovo->esq = noVelho->esq;
+    noNovo->dir = noVelho->dir;
+    noNovo->pai = noVelho->pai;
+
+    // verificações de no nulo
+    if (noNovo == noVelho->esq) {
+        noNovo->esq = NULL;
+    }
+
+    if (noNovo == noVelho->dir) {
+        noNovo->dir = NULL;
+    }
+
+    printf("Substituindo (%d) por (%d)!\n\n", noVelho->valor, noNovo->valor);
+}
 
 tipoNo* buscarNo(tipoNo *raiz, int valor) {
     // Se o nó for NULL, o valor não está na árvore
@@ -233,72 +281,131 @@ tipoNo* buscarNo(tipoNo *raiz, int valor) {
 }
 
 void verificarCorrecoesRemocao(tipoArvore *arvore, tipoNo *no) {
+    if (no == NULL && no == arvore->raiz) {
+        printf("A árvore foi zerada\n");
+        return;
+    }
+
     while (no != arvore->raiz && no->cor == NEGRO) {
+        // Nó é filho esquerdo
         if (no == no->pai->esq) {
+            printf("O substituto é filho esquerdo\n");
             tipoNo *irmao = no->pai->dir;
 
             // Caso 1: O irmão do nó é rubro
             if (irmao->cor == RUBRO) {
+                printf("O irmão (%d) de (%d) é rubro\n", irmao->valor, no->valor);
+                printf("O irmão vira negro\n");
+                printf("O pai vira rubro\n");
                 irmao->cor = NEGRO;
                 no->pai->cor = RUBRO;
+                printf("Rotacionando o pai (%d) para esquerda\n", no->pai->valor);
                 rotacaoEsquerda(arvore, no->pai);
                 irmao = no->pai->dir;
+                printf("irmao virou (%d)\n", irmao->valor);
             }
 
-            // Caso 2: O irmão e os filhos do irmão são negros
-            if (irmao->esq->cor == NEGRO && irmao->dir->cor == NEGRO) {
+            // Caso 2: Os filhos do irmão são negros
+            if ((irmao->esq == NULL || irmao->esq->cor == NEGRO) &&
+                (irmao->dir == NULL || irmao->dir->cor == NEGRO)) {
+                printf("Os dois filhos do irmão são negros\n");
+                printf("Irmao virou rubro\n");
                 irmao->cor = RUBRO;
                 no = no->pai;
+                printf("Atualizou nó para (%d)\n", no->valor);
             } else {
-                // Caso 3: O irmão é negro, e o filho esquerdo é rubro
-                if (irmao->dir->cor == NEGRO) {
-                    irmao->esq->cor = NEGRO;
+                // Caso 3: O filho direito é negro
+                if (irmao->dir == NULL || irmao->dir->cor == NEGRO) {
+                    printf("O filho direito do irmão é negro\n");
+                    if (irmao->esq != NULL) irmao->esq->cor = NEGRO;
+                    printf("Filho esquerdo virou negro\n");
+                    printf("Irmão virou rubro\n");
                     irmao->cor = RUBRO;
+                    printf("Rotacionando irmão para direita\n");
                     rotacaoDireita(arvore, irmao);
                     irmao = no->pai->dir;
+                    printf("Atualizou imão para (%d)\n", irmao->valor);
                 }
 
-                // Caso 4: O irmão é negro, e o filho direito é rubro
+                // Caso 4: Trocar cores
                 irmao->cor = no->pai->cor;
+                printf("Irmão copia a cor do pai (%s)\n", irmao->cor == NEGRO ? "negro" : "rubro");
                 no->pai->cor = NEGRO;
-                irmao->dir->cor = NEGRO;
+                printf("Pai vira negro");
+                printf("Filho direito do irmão vira negro");
+                if (irmao->dir != NULL) irmao->dir->cor = NEGRO;
+                printf("Rotacionando pai para esquerda\n");
                 rotacaoEsquerda(arvore, no->pai);
                 no = arvore->raiz;
+                printf("Atualizou nó para raiz");
             }
         } else {
+            printf("O substituto é filho Direito");
             // Casos simétricos para o irmão à esquerda
             tipoNo *irmao = no->pai->esq;
 
             if (irmao->cor == RUBRO) {
+                printf("O irmão (%d) de (%d) é rubro\n", irmao->valor, no->valor);
+                printf("O irmão vira negro\n");
+                printf("O pai vira rubro\n");
                 irmao->cor = NEGRO;
                 no->pai->cor = RUBRO;
+                printf("Rotacionando o pai (%d) para direita\n", no->pai->valor);
                 rotacaoDireita(arvore, no->pai);
                 irmao = no->pai->esq;
+                printf("irmao virou (%d)\n", irmao->valor);
             }
 
-            if (irmao->esq->cor == NEGRO && irmao->dir->cor == NEGRO) {
+            if ((irmao->esq == NULL || irmao->esq->cor == NEGRO) &&
+                (irmao->dir == NULL || irmao->dir->cor == NEGRO)) {
+                printf("Os dois filhos do irmão são negros\n");
+                printf("Irmao virou rubro\n");
                 irmao->cor = RUBRO;
                 no = no->pai;
+                printf("Atualizou nó para (%d)\n", no->valor);
             } else {
-                if (irmao->esq->cor == NEGRO) {
-                    irmao->dir->cor = NEGRO;
+                if (irmao->esq == NULL || irmao->esq->cor == NEGRO) {
+                    printf("O filho direito do irmão é negro\n");
+                    if (irmao->dir != NULL) irmao->dir->cor = NEGRO;
+                    printf("Filho direito virou negro\n");
+                    printf("Irmão virou rubro\n");
                     irmao->cor = RUBRO;
+                    printf("Rotacionando irmão para esquerda\n");
                     rotacaoEsquerda(arvore, irmao);
                     irmao = no->pai->esq;
+                    printf("Atualizou imão para (%d)\n", irmao->valor);
                 }
 
                 irmao->cor = no->pai->cor;
+                printf("Irmão copia a cor do pai (%s)\n", irmao->cor == NEGRO ? "negro" : "rubro");
                 no->pai->cor = NEGRO;
-                irmao->esq->cor = NEGRO;
+                printf("Pai vira negro");
+                printf("Filho esquerdo do irmão vira negro");
+                if (irmao->esq != NULL) irmao->esq->cor = NEGRO;
+                printf("Rotacionando pai para direita\n");
                 rotacaoDireita(arvore, no->pai);
                 no = arvore->raiz;
+                printf("Atualizou nó para raiz");
             }
         }
     }
 
-    no->cor = NEGRO;
+    if (no != NULL) {
+        no->cor = NEGRO;
+    }
+    
+    printf("Fim das verificações\n\n");
 }
 
+tipoNo* criarNoNegroNulo(tipoArvore *arvore) {
+    tipoNo *nuloNegro = (tipoNo*) malloc(sizeof(tipoNo));
+    nuloNegro->cor = NEGRO;
+    nuloNegro->valor = -1;  // Valor fictício para representar nó nulo
+    nuloNegro->pai = NULL;
+    nuloNegro->esq = NULL;
+    nuloNegro->dir = NULL;
+    return nuloNegro;
+}
 
 void removerNo(tipoArvore *arvore, int valor) {
     if (arvore->raiz == NULL) {
@@ -307,58 +414,75 @@ void removerNo(tipoArvore *arvore, int valor) {
     }
 
     // Buscar o nó com o valor fornecido
-    tipoNo *no = buscarNo(arvore->raiz, valor);
+    tipoNo *noRemovido = buscarNo(arvore->raiz, valor);
 
     // Nó não encontrado
-    if (no == NULL) {
+    if (noRemovido == NULL) {
         printf("Valor %d não encontrado na árvore!\n", valor);
         return;
     }
 
     // Imprime o valor do nó que será removido
-    printf("Nó a ser removido = %d\n", no->valor);
+    printf("\nNó a ser removido = %d\n", noRemovido->valor);
+
+    // Caso 1: O nó removido é folha e vermelho
+    if (noRemovido->esq == NULL && noRemovido->dir == NULL && noRemovido->cor == RUBRO) {
+        printf("(%d) é folha e vermelho\n", noRemovido->valor);
+        substituirNo(arvore, noRemovido, NULL);
+        free(noRemovido);
+        return;
+    }
+
+    // Caso 2: O nó removido é folha e negro
+    if (noRemovido->esq == NULL && noRemovido->dir == NULL) {
+        printf("(%d) não tem filhos\n", noRemovido->valor);
+        printf("(%d) é folha e negro\n", noRemovido->valor);
+        tipoNo *noFantasma = criarNoNegroNulo(arvore);
+        noFantasma->pai = noRemovido->pai;
+        noFantasma->esq = noRemovido->esq;
+        noFantasma->dir = noRemovido->dir;
+        noFantasma->cor = NEGRO;
+        substituirNo(arvore, noRemovido, noFantasma);
+        verificarCorrecoesRemocao(arvore, noFantasma);
+        noFantasma->esq = NULL;
+        noFantasma->dir = NULL;
+        if (noFantasma->pai->esq == noFantasma) {
+            noFantasma->pai->esq = NULL;
+        } else {
+            noFantasma->pai->dir = NULL;
+        }
+        noFantasma->pai = NULL;
+        free(noFantasma);
+        free(noRemovido);
+        return;
+    }
 
     tipoNo *substituto;
-    int corOriginal = no->cor;  // Armazenar a cor original para verificação posterior
+    int corOriginal = noRemovido->cor;  // Armazenar a cor original para verificação posterior
 
-    if (no->esq == NULL) {
-        // Caso 1: O nó não tem filho esquerdo
-        substituto = no->dir;
-        substituirNo(arvore, no, substituto);
-    } else if (no->dir == NULL) {
-        // Caso 2: O nó não tem filho direito
-        substituto = no->esq;
-        substituirNo(arvore, no, substituto);
-    } else {
-        // Caso 3: O nó tem dois filhos
-        tipoNo *sucessor = minimo(no->dir);  // Encontra o sucessor
-        corOriginal = sucessor->cor;         // Atualiza a cor original para o sucessor
-        substituto = sucessor->dir;          // O substituto pode ser NULL ou um filho direito
-        
-        if (sucessor->pai == no) {
-            if (substituto != NULL) {
-                substituto->pai = sucessor;
-            }
-        } else {
-            substituirNo(arvore, sucessor, substituto);
-            sucessor->dir = no->dir;
-            sucessor->dir->pai = sucessor;
-        }
-
-        substituirNo(arvore, no, sucessor);
-        sucessor->esq = no->esq;
-        sucessor->esq->pai = sucessor;
-        sucessor->cor = no->cor;  // Sucessor herda a cor do nó removido
+    // Caso 3: O nó removido tem apenas filho direito
+    if (noRemovido->esq == NULL) {
+        printf("(%d) não tem filho esquerdo\n", noRemovido->valor);
+        printf("Procurando na arvore o menor valor da subarvore à direita\n");
+        substituto = minimo(noRemovido->dir);
+    }
+    // Caso 4: O nó removido tem filho esquerdo
+    else if (noRemovido->esq != NULL) {
+        printf("(%d) tem filho esquerdo\n", noRemovido->valor);
+        printf("Procurando na arvore o maior valor da subarvore à esquerda\n");
+        substituto = maximo(noRemovido->esq);
     }
 
-    printf("removendo (%d) \n\n", no->valor);
-    
-    free(no);  // Libera a memória do nó removido
+    printf("Substituto = %d\n", substituto->valor);
+    substituirNo(arvore, noRemovido, substituto);
 
-    // Somente realizar correções se o nó removido ou o sucessor eram negros
-    if (corOriginal == NEGRO && substituto != NULL) {
+    if (corOriginal == NEGRO && substituto->cor == RUBRO) {
+        substituto->cor = NEGRO;
+    } else if (corOriginal == NEGRO) {
         verificarCorrecoesRemocao(arvore, substituto);
     }
+
+    free(noRemovido);
 }
 
 // Mostrando no sentido LNR
