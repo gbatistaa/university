d9917d71c91c655d7cb4ec9ac190a8fac6b76e65
gabrielbatistabarbosa4 <gabrielbatistabarbosa4@gmail.com>
2024-10-29 12:11:45 +0000

    addition of basic binary search tree operations

--- 3P/prova-2/sum-leaf.c
+++ 3P/prova-2/sum-leaf.c
@@ -9,3 +9,64 @@ typedef struct node
   struct node *rightChild;
 } BinaryNode;
 
+BinaryNode *createBinaryNode(int value) {
+  BinaryNode *newNode = (BinaryNode*)malloc(sizeof(BinaryNode));
+  newNode->value = value;
+  newNode->leftChild = NULL;
+  newNode->rightChild = NULL;
+  return newNode;
+}
+
+int printInOrder(BinaryNode *root) {
+  if(root == NULL) {
+    return EXIT_SUCCESS;
+  } else {
+    printInOrder(root->leftChild);
+    printf("%d", root->value);
+    printInOrder(root->rightChild);
+  }
+}
+
+BinaryNode *insertBinaryNode(BinaryNode *root, int value) {
+  BinaryNode *newNode = createBinaryNode(value);
+
+  if (root == NULL) {
+    return newNode;
+  }
+
+  if (value < root->value) {
+    root->leftChild = insertBinaryNode(root->leftChild, value);
+  } else if (value > root->value) {
+    root->rightChild = insertBinaryNode(root->rightChild, value);
+  }
+
+  return root;
+}
+
+int main() {
+  int op, value;
+  BinaryNode *root = NULL;
+
+  do {
+    printf("** Árvore Binária de Busca **\n");
+    printf("1. Inserir\n");
+    printf("2. Exibir\n");
+    printf("3. Sair\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &op);
+
+    switch(op) {
+      case 0:
+        printf("Fim da execução\n");
+        break;
+      case 1:
+        printf("Digite um valor: ");
+        scanf(" %d", &value);
+        root = insertBinaryNode(root, value);
+        break;
+      case 2:
+        printf("Imprimindo árvore em ordem: \n");
+        
+    }
+  } while (op != 0);
+}
