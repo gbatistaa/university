5c99f262710c0081f96d61d7d731f7bb001dffbc
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-01-10 08:07:31 -0300

    feat: Add the built-from-scratch hashmap data structure template class

--- 4P/paa/u1/poxim-tech.cpp
+++ 4P/paa/u1/poxim-tech.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
@@ -6,10 +7,171 @@
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <ostream>
 #include <string>
 #include <vector>
 
 using namespace std;
+
+template <typename Value> class hashmap {
+private:
+  size_t elements_num;
+  float density;
+
+  typedef struct key_value {
+    string key;
+    Value value;
+  } key_value;
+
+  class linked_pair {
+  public:
+    key_value pair;
+    linked_pair *next;
+  };
+
+  bool isGreaterThan(float a, float b, float e = 1e-6) {
+    return (a > b) && fabs(a - b) > e;
+  }
+
+  int re_hash() {
+    // Doubleing the size of the hashmap array:
+    size_t prev_map_size = map_size;
+    map_size = (size_t)map_size * sqrt(5);
+    linked_pair *new_map = (linked_pair *)calloc(map_size, sizeof(linked_pair));
+
+    // Re-hashing all the elements:
+    for (int i = 0; i < prev_map_size; i++) {
+
+      // Ignoring the empty spaces:
+      if (map[i].pair.key == "")
+        continue;
+
+      int hash_index = hash_function(map[i].pair.key, map_size);
+
+      // Copying the current previous map element:
+      linked_pair *copy_pair = (linked_pair *)malloc(sizeof(linked_pair));
+      copy_pair->pair.key = map[i].pair.key;
+      copy_pair->pair.value = map[i].pair.value;
+      copy_pair->next = map[i].next;
+
+      // Colision treatment:
+      if (new_map[hash_index].pair.key != "") {
+        linked_pair *curr_pair = &new_map[hash_index];
+        // Scanning until it reaches the last element of the linked list:
+        while (curr_pair->next != nullptr) {
+          curr_pair = curr_pair->next;
+        }
+        curr_pair->next = copy_pair;
+      } else {
+        new_map[hash_index] = *copy_pair;
+      }
+    }
+    map = new_map;
+
+    return EXIT_SUCCESS;
+  }
+
+  int hash_function(string key, int list_size) {
+    int hash_index = 0;
+    for (char letter : key)
+      hash_index += letter;
+    return hash_index % list_size;
+  }
+
+public:
+  size_t map_size;
+
+  // Hash Map constructor:
+  hashmap() {
+    elements_num = 0;
+    map_size = 2;
+    map = (linked_pair *)calloc(map_size, sizeof(linked_pair));
+  };
+
+  linked_pair *map;
+
+  linked_pair *hash_insert(string key, Value value) {
+    elements_num++;
+
+    // Storing the new key value pair:
+    key_value kv;
+    kv.key = key;
+    kv.value = value;
+
+    linked_pair *new_pair = (linked_pair *)malloc(sizeof(linked_pair));
+    new_pair->pair = kv;
+    new_pair->next = nullptr;
+
+    // Case when the hashmap has 0 elements
+    density = (float)elements_num / map_size;
+
+    // Condition to verify if the resizing is needed (more than 75%):
+    if (isGreaterThan(density, 0.75)) {
+      re_hash();
+    }
+
+    int hash_index = hash_function(key, map_size);
+
+    // Case that the hash index collides:
+    if (map[hash_index].pair.key != "") {
+      linked_pair *curr_pair = &map[hash_index];
+      while (curr_pair->next != nullptr && curr_pair->pair.key != key) {
+        curr_pair = curr_pair->next;
+      }
+      if (curr_pair->next == nullptr) {
+        curr_pair->next = new_pair;
+      } else {
+        curr_pair->next->pair.value = value;
+      }
+    } else {
+      map[hash_index] = *new_pair;
+    }
+    return map;
+  }
+
+  int print_hash_map() {
+    for (int i = 0; i < map_size; i++) {
+      if (map[i].pair.key != "") {
+        cout << map[i].pair.key << " | " << map[i].pair.value;
+        linked_pair *curr_pair = &map[i];
+
+        // Scanning all of the collided pairs:
+        while (curr_pair->next != nullptr) {
+          cout << " --> " << curr_pair->next->pair.key << " | "
+               << curr_pair->next->pair.value;
+          curr_pair = curr_pair->next;
+        }
+        cout << endl;
+      }
+    }
+    return EXIT_SUCCESS;
+  }
+
+  Value at(string key) {
+    int hash_index = hash_function(key, map_size);
+
+    if (map[hash_index].pair.key == key)
+      return map[hash_index].pair.value;
+    else {
+      linked_pair *curr_pair = &map[hash_index];
+      try {
+        if (curr_pair->next != nullptr) {
+          while (curr_pair->next->pair.key != key) {
+            if (curr_pair->next == nullptr)
+              break;
+            else
+              curr_pair = curr_pair->next;
+          }
+          return curr_pair->next->pair.value;
+        } else
+          throw;
+      } catch (const exception &e) {
+        cerr << e.what() << endl;
+      }
+    }
+    return EXIT_FAILURE;
+  }
+};
 
 enum Irregularity {
   WEIGHT,
@@ -19,21 +181,20 @@ enum Irregularity {
 class Container {
 public:
   string code;
-
-public:
   string cnpj;
-
-public:
   int weight;
 };
 
 class Irregular : public Container {
 public:
   Irregularity irregularity;
-
-public:
   string irregularityMessage;
 };
+
+typedef struct containerList {
+  Container *list;
+  int size;
+} ContainerList;
 
 bool isGreaterThan(float a, float b, float e = 1e-6) {
   return (a > b) && fabs(a - b) > e;
@@ -98,57 +259,6 @@ float calcContainerWeightDifPercent(Container container,
   }
 }
 
-int writeContainerProp(string *props[], char character, int &propSts) {
-  if (character == ' ') {
-    propSts++;
-  } else {
-    int i = propSts;
-    *props[i] += character;
-  }
-  return EXIT_SUCCESS;
-}
-
-int readInputAndCreateContainerLists(fstream &file,
-                                     vector<Container> *contPointers[]) {
-  string fileLine;
-  int currContainerIndex = 0;
-
-  // Ignoring the first line of the file
-  getline(file, fileLine);
-
-  while (getline(file, fileLine)) {
-    int lineSize = fileLine.length();
-
-    // Verification to create the next container list
-    if (lineSize <= 3) {
-      currContainerIndex++;
-      continue;
-    }
-
-    Container newContainer;
-    string newCnpj, newCode, newWeight;
-    int newWeightNumeric, propStatus = 0;
-    string *props[3] = {&newCode, &newCnpj, &newWeight};
-
-    // Reading the file, separating in props and writing in the string
-    // variables
-    for (int i = 0; i < lineSize; i++) {
-      writeContainerProp(props, fileLine.at(i), propStatus);
-    }
-    newWeightNumeric = stoi(newWeight);
-
-    // Writing the colected data in the props of the new container
-    newContainer.code = newCode;
-    newContainer.cnpj = newCnpj;
-    newContainer.weight = newWeightNumeric;
-
-    // Adding the new container in the containers list:
-    contPointers[currContainerIndex]->push_back(newContainer);
-  }
-
-  return EXIT_SUCCESS;
-}
-
 vector<Irregular>
 createIrregularContainersList(map<string, Container> fiscalizeds,
                               vector<Container> duplicateds) {
@@ -167,7 +277,7 @@ createIrregularContainersList(map<string, Container> fiscalizeds,
       newCnpjIrr.cnpj = duplicated.cnpj;
       newCnpjIrr.weight = duplicated.weight;
       newCnpjIrr.irregularity = CNPJ;
-      newCnpjIrr.irregularityMessage = irrMsg;
+      newCnpjIrr.irregularityMessage = duplicated.code + ":" + irrMsg;
       irregulars.push_back(newCnpjIrr);
     }
 
@@ -182,7 +292,7 @@ createIrregularContainersList(map<string, Container> fiscalizeds,
       newWeightIrr.cnpj = duplicated.cnpj;
       newWeightIrr.weight = duplicated.weight;
       newWeightIrr.irregularity = WEIGHT;
-      newWeightIrr.irregularityMessage = irrMsg;
+      newWeightIrr.irregularityMessage = duplicated.code + ":" + irrMsg;
       irregulars.push_back(newWeightIrr);
     }
   }
@@ -252,6 +362,78 @@ void sortIrregularContainers(vector<Irregular> &vec,
   }
 }
 
+int writeContainerProp(string *props[], char character, int &propSts) {
+  if (character == ' ') {
+    propSts++;
+  } else {
+    int i = propSts;
+    *props[i] += character;
+  }
+  return EXIT_SUCCESS;
+}
+
+int readInputAndCreateContainerLists(fstream &file,
+                                     ContainerList **contPointers) {
+  string fileLine;
+  int currContainerIndex = 0;
+
+  // Ignoring the first line of the file
+  getline(file, fileLine);
+
+  while (getline(file, fileLine)) {
+    int lineSize = fileLine.length();
+
+    // Verification to create the next container list
+    if (lineSize <= 3) {
+      currContainerIndex++;
+      continue;
+    }
+
+    Container newContainer;
+    string newCnpj, newCode, newWeight;
+    int newWeightNumeric, propStatus = 0;
+    string *props[3] = {&newCode, &newCnpj, &newWeight};
+
+    // Reading the file, separating in props and writing in the string
+    // variables
+    for (int i = 0; i < lineSize; i++) {
+      writeContainerProp(props, fileLine.at(i), propStatus);
+    }
+    newWeightNumeric = stoi(newWeight);
+
+    // Writing the colected data in the props of the new container:
+    newContainer.code = newCode;
+    newContainer.cnpj = newCnpj;
+    newContainer.weight = newWeightNumeric;
+
+    // Incrementing the new containers list size and storing the previous value:
+    int prevListSize = contPointers[currContainerIndex]->size;
+
+    contPointers[currContainerIndex]->size++;
+
+    int newListSize = contPointers[currContainerIndex]->size;
+
+    // Adding the new container in the containers list:
+    Container *newArray = (Container *)malloc(sizeof(Container) * newListSize);
+    if (newArray == NULL) {
+      cerr << "Error on memory allocation\n";
+      exit(EXIT_FAILURE);
+    }
+
+    for (int x = 0; x < prevListSize; x++) {
+      newArray[x] = contPointers[currContainerIndex]->list[x];
+    }
+
+    // Reallocating the pointer to the new array:
+    contPointers[currContainerIndex]->list = newArray;
+
+    // Adding the new element to the last position of the container list:
+    contPointers[currContainerIndex]->list[newListSize - 1] = newContainer;
+  }
+
+  return EXIT_SUCCESS;
+}
+
 int main() {
   fstream input("poxim-tech.txt");
   if (!input.is_open()) {
@@ -259,28 +441,54 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  vector<Container> registeredContainers;
-  vector<Container> fiscalizedContainers;
+  ContainerList *registeredContainers =
+      (ContainerList *)malloc(sizeof(ContainerList));
+  registeredContainers->size = 0;
+  registeredContainers->list =
+      (Container *)malloc(sizeof(Container) * registeredContainers->size);
 
-  vector<Container> *containersPointers[2] = {&registeredContainers,
-                                              &fiscalizedContainers};
+  ContainerList *fiscalizedContainers =
+      (ContainerList *)malloc(sizeof(ContainerList));
+  fiscalizedContainers->size = 0;
+  fiscalizedContainers->list =
+      (Container *)malloc(sizeof(Container) * fiscalizedContainers->size);
+
+  ContainerList *containersPointers[2] = {registeredContainers,
+                                          fiscalizedContainers};
 
   readInputAndCreateContainerLists(input, containersPointers);
 
-  map<string, Container> fiscalizedsMap =
-      createContainerMap(fiscalizedContainers);
-
-  vector<Container> duplicatedContainers =
-      filterRegisteredContainersForInspection(registeredContainers,
-                                              fiscalizedsMap);
-  vector<Irregular> irregulars =
-      createIrregularContainersList(fiscalizedsMap, duplicatedContainers);
-
-  sortIrregularContainers(irregulars, fiscalizedsMap, 0, irregulars.size() - 1);
-
-  for (int i = 0; i < irregulars.size(); i++) {
-    cout << irregulars.at(i).irregularityMessage << "\n";
+  cout << "----------Registered Containers----------\n\n";
+  for (int i = 0; i < registeredContainers->size; i++) {
+    cout << "Code: " << registeredContainers->list[i].code << "\n";
+    cout << "CNPJ: " << registeredContainers->list[i].cnpj << "\n";
+    cout << "Weight: " << registeredContainers->list[i].weight << " kg"
+         << "\n\n";
   }
+
+  cout << "----------Fiscalized Containers----------\n\n";
+  for (int i = 0; i < fiscalizedContainers->size; i++) {
+    cout << "Code: " << fiscalizedContainers->list[i].code << "\n";
+    cout << "CNPJ: " << fiscalizedContainers->list[i].cnpj << "\n";
+    cout << "Weight: " << fiscalizedContainers->list[i].weight << " kg"
+         << "\n\n";
+  }
+
+  // map<string, Container> fiscalizedsMap =
+  //     createContainerMap(fiscalizedContainers);
+
+  // vector<Container> duplicatedContainers =
+  //     filterRegisteredContainersForInspection(registeredContainers,
+  //                                             fiscalizedsMap);
+  // vector<Irregular> irregulars =
+  //     createIrregularContainersList(fiscalizedsMap, duplicatedContainers);
+
+  // sortIrregularContainers(irregulars, fiscalizedsMap, 0, irregulars.size() -
+  // 1);
+
+  // for (int i = 0; i < irregulars.size(); i++) {
+  //   cout << irregulars.at(i).irregularityMessage << "\n";
+  // }
 
   return EXIT_SUCCESS;
 }
