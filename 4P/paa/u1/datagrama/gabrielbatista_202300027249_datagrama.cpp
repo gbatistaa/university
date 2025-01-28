b3e6ac575617140ab228c153b6d798b249c1589a
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-01-28 10:25:35 -0300

    feat: Create the packages list and the package classes

--- 4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp
+++ 4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp
@@ -1,7 +1,24 @@
+#include <cmath>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
+
+class Package {
+  int code;
+  int size;
+  char *bytes_list[2];
+};
+
+class PackageList {
+public:
+  int total_pkgs;
+  int pkgs_per_read;
+  Package *pkgs;
+};
 
 int heapify(int array[], int size, int root) {
   int bigger = root;
@@ -24,29 +41,47 @@ int heapify(int array[], int size, int root) {
   return EXIT_SUCCESS;
 }
 
-/*
-  - O heap começa do primeiro nó não folha até a raiz
-  - Por isso deve-se iterar do índice desse número até 0 (raiz)
-*/
-
-int buildHeap(int array[], int size) {
+int build_heap(int array[], int size) {
   for (int i = (size / 2) - 1; i >= 0; i--) {
     heapify(array, size, i);
   }
   return EXIT_SUCCESS;
 }
 
-int main() {
-  int array[6] = {42, 23, 95, 19, 56, 33};
-  int size = sizeof(array) / sizeof(array[0]);
+int read_file(ifstream &input) {
+  string line;
 
-  buildHeap(array, size);
-
-  cout << "Max-Heap: ";
-  for (int i = 0; i < size; i++) {
-    cout << array[i] << " ";
+  // Reading ther first line and getting the pkgs total and pkgs quantity
+  if (!getline(input, line)) {
+    cerr << "Error on the file reading" << endl;
   }
-  cout << endl;
+
+  istringstream iss(line);
+
+  return EXIT_SUCCESS;
+}
+
+int main(int args, char *argv[3]) {
+  ifstream input(argv[1]);
+  ofstream output(argv[2]);
+
+  if (!input.is_open()) {
+    cerr << "Erro ao abrir input" << endl;
+    return EXIT_FAILURE;
+  }
+
+  cout << "Input aberto com sucesso!\n" << endl;
+
+  if (!output.is_open()) {
+    cerr << "Erro ao abrir output" << endl;
+    return EXIT_FAILURE;
+  }
+
+  PackageList package_list = PackageList();
+
+  read_file(input);
+
+  cout << "Output aberto com sucesso!\n" << endl;
 
   return EXIT_SUCCESS;
 }
