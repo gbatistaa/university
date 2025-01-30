1127bfeb2362a7034aae9266d6eee977479d7c2f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-01-29 23:43:01 -0300

    chore: Change the logic from show on terminal to write in the output file

--- 4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp
+++ 4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp
@@ -1,6 +1,4 @@
 #include <algorithm>
-#include <codecvt>
-#include <cstddef>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -25,53 +23,49 @@ public:
   Package *pkgs;
 };
 
-// Function to adjust a node to maintain the max-heap property
-void max_heapify(Package array[], int size, int root) {
+int max_heapify(Package array[], int size, int root) {
   int largest = root;
   int left = 2 * root + 1;
   int right = 2 * root + 2;
 
-  // If left child exists and is greater than root
   if (left < size && array[left].code > array[largest].code) {
     largest = left;
   }
 
-  // If right child exists and is greater than the current largest
   if (right < size && array[right].code > array[largest].code) {
     largest = right;
   }
 
-  // If largest is not root, swap and heapify recursively
   if (largest != root) {
     swap(array[root], array[largest]);
     max_heapify(array, size, largest);
   }
+  return EXIT_SUCCESS;
 }
 
-// Function to build a max-heap
-void build_max_heap(Package array[], int size) {
+int build_max_heap(Package array[], int size) {
   for (int i = size / 2 - 1; i >= 0; i--) {
     max_heapify(array, size, i);
   }
+  return EXIT_SUCCESS;
 }
 
-// Heap Sort: Sorts array in ascending order using a max-heap
-void heap_sort(Package array[], int size) {
-  build_max_heap(array, size); // Step 1: Build a max-heap
+int heap_sort(Package array[], int size) {
+  build_max_heap(array, size);
 
-  // Step 2: Extract elements from heap one by one
   for (int i = size - 1; i > 0; i--) {
-    swap(array[0], array[i]); // Move current root to end
-    max_heapify(array, i, 0); // Restore heap property on the reduced heap
+    swap(array[0], array[i]);
+    max_heapify(array, i, 0);
   }
+  return EXIT_SUCCESS;
 }
 
-int print_pkg_bytes(Package pkg) {
-  cout << "|";
+int write_pkg_bytes(ofstream &output, Package pkg) {
+  output << "|";
   for (int i = 0; i < pkg.size - 1; i++) {
-    cout << pkg.bytes_list[i] << ",";
+    output << pkg.bytes_list[i] << ",";
   }
-  cout << pkg.bytes_list[pkg.size - 1];
+  output << pkg.bytes_list[pkg.size - 1];
   return EXIT_SUCCESS;
 }
 
@@ -80,24 +74,22 @@ int write_sorted_pkgs(ofstream &output, Package *pkgs, int list_size,
   int expected_pkg = 0, curr_pkg = 0, wait_end = -1, curr_wait = 0,
       read_pkgs = 0;
   Package wait_list[list_size];
-
-  // Iteration in all the packages on the packages list;
   for (int i = 0; i < list_size; i++) {
     read_pkgs++;
     wait_list[++wait_end] = pkgs[i];
     if (read_pkgs % pkgs_per_read == 0) {
-      cout << "\n";
+      if (read_pkgs > pkgs_per_read)
+        output << "\n";
       for (int j = 0; j <= wait_end; j++) {
         heap_sort(wait_list, wait_end + 1);
         if (wait_list[j].code == expected_pkg) {
-          print_pkg_bytes(wait_list[j]);
+          write_pkg_bytes(output, wait_list[j]);
           expected_pkg++;
         }
       }
-      cout << "|";
+      output << "|";
     }
   }
-  cout << endl;
   return EXIT_SUCCESS;
 }
 
@@ -144,13 +136,11 @@ int main(int args, char *argv[3]) {
     cerr << "Erro ao abrir input" << endl;
     return EXIT_FAILURE;
   }
-  cout << "Input aberto com sucesso!\n" << endl;
 
   if (!output.is_open()) {
     cerr << "Erro ao abrir output" << endl;
     return EXIT_FAILURE;
   }
-  cout << "Output aberto com sucesso!\n" << endl;
 
   PackageList *package_list = new PackageList();
 
