67f0676523bea942627e2551e60aa50669207fa7
gbatistaa <gbatistadev@gmail.com>
2025-02-06 18:34:20 +0000

    chore: Add opening the input and th output files

--- 4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp
+++ 4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -19,6 +20,19 @@ public:
 };
 
 int main(int args, char *argv[]) {
+
+  ifstream input(argv[1]);
+  ofstream output(argv[2]);
+
+  if (!input.is_open()) {
+    cerr << "Error on input file opening" << endl;
+    return EXIT_FAILURE;
+  }
+
+  if (!output.is_open()) {
+    cerr << "Error on output file opening" << endl;
+    return EXIT_FAILURE;
+  }
 
   cout << "Hello World!" << endl;
   return EXIT_SUCCESS;
