a8da38c52b518c424f434ceaa4fff1497f281e07
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-17 10:58:03 -0300

    feat: create optimized function to solve the 3D backpack problem

--- 4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp
+++ 4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp
@@ -11,8 +11,8 @@ using namespace std::chrono;
 class Vehicle {
 public:
   string sign;
-  int weight = 0;
-  int volume = 0;
+  int max_weight = 0;
+  int max_volume = 0;
 };
 
 class VehicleList {
@@ -55,8 +55,8 @@ int read_file(ifstream &input, VehicleList *&vehicle_list,
     }
     // Put the line data on the vehicles list (tested):
     iss >> vehicle_list->list[v].sign;
-    iss >> vehicle_list->list[v].weight;
-    iss >> vehicle_list->list[v++].volume;
+    iss >> vehicle_list->list[v].max_weight;
+    iss >> vehicle_list->list[v++].max_volume;
   }
   // Reading and allocating memory for the package list:
 
@@ -73,6 +73,38 @@ int read_file(ifstream &input, VehicleList *&vehicle_list,
     iss >> package_list->list[p++].volume;
   }
 
+  return EXIT_SUCCESS;
+}
+
+int maximize_vehicle_value(PackageList *package_list, Vehicle vehicle,
+                           string &output_string) {
+  const int packages = package_list->size + 1;
+  const int max_weight = vehicle.max_weight + 1;
+  const int max_volume = vehicle.max_volume + 1;
+
+  // Initializing the backpack with null values:
+  float **backpack = new float *[max_weight];
+  for (int w = 0; w < max_weight; w++) {
+    backpack[w] = new float[max_volume]{0};
+  }
+
+  for (int i = 1; i < packages; i++) {
+    for (int w = 0; w < max_weight; w++) {
+      for (int v = 0; v < max_volume; v++) {
+        if (package_list->list[i - 1].weight <= w &&
+            package_list->list[i - 1].volume <= v) {
+          backpack[w][v] =
+              backpack[w][v] < backpack[w - package_list->list[i - 1].weight]
+                                       [v - package_list->list[i - 1].volume] +
+                                   package_list->list[i - 1].value
+                  ? backpack[w][v]
+                  : backpack[w - package_list->list[i - 1].weight]
+                            [v - package_list->list[i - 1].volume] +
+                        package_list->list[i - 1].value;
+        }
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
 
@@ -98,8 +130,13 @@ int main(int args, char *argv[]) {
 
   PackageList *package_list = new PackageList();
   VehicleList *vehicle_list = new VehicleList();
+  string output_string;
 
   read_file(input, vehicle_list, package_list);
+
+  for (int i = 0; i < vehicle_list->size; i++) {
+    maximize_vehicle_value(package_list, vehicle_list->list[i], output_string);
+  }
 
   auto end = high_resolution_clock::now();
   duration<double> duration = end - start;
