01736a8dc1e76ed54172abe696346ffb89db80ed
Gabriel Barbosa <gbatistadev@gmail.com>
2025-02-28 09:57:02 -0300

    feat: create the read file system

--- 4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp
+++ 4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp
@@ -1,11 +1,26 @@
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <ostream>
 
 using namespace std;
 
-int main() {
-  cout << "Hello World!" << endl;
+int main(int argc, char *argv[]) {
+
+  ifstream input(argv[1]);
+  ofstream output(argv[2]);
+
+  if (!input.is_open()) {
+    cerr << "Erro ao abrir input" << endl;
+    return EXIT_FAILURE;
+  }
+  cout << "Input aberto com sucesso!" << endl;
+
+  if (!output.is_open()) {
+    cerr << "Erro ao abrir output" << endl;
+    return EXIT_FAILURE;
+  }
+  cout << "Output aberto com sucesso!\n" << endl;
 
   return EXIT_SUCCESS;
 }
