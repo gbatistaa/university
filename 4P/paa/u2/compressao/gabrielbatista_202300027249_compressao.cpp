2c41542094aec39a75f3a77ab084b3735c505d2a
Gabriel Barbosa <gbatistadev@gmail.com>
2025-02-28 11:35:09 -0300

    feat: create the read file function

--- 4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp
+++ 4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp
@@ -2,8 +2,47 @@
 #include <fstream>
 #include <iostream>
 #include <ostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
 
 using namespace std;
+
+class Data {
+public:
+  string *bytes = nullptr;
+  int size = 0;
+};
+
+class DataList {
+public:
+  Data *datas = nullptr;
+  int size = 0;
+};
+
+int read_file(ifstream &input, DataList *&data_list) {
+  string line = "";
+
+  // Allocating memoy for the datas list:
+  getline(input, line);
+  int data_list_size = stoi(line);
+  data_list->size = data_list_size;
+  data_list->datas = new Data[data_list_size];
+
+  // Allocating memory for each data set:
+  for (int i = 0; getline(input, line); i++) {
+    istringstream iss(line);
+    iss >> data_list->datas[i].size;
+    data_list->datas[i].bytes = new string[data_list->datas[i].size];
+
+    int j = 0;
+    while (iss >> data_list->datas[i].bytes[j]) {
+      j++;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -21,6 +60,10 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
   cout << "Output aberto com sucesso!\n" << endl;
+
+  DataList *data_list = new DataList();
+
+  read_file(input, data_list);
 
   return EXIT_SUCCESS;
 }
