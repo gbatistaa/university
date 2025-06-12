544237e9cc978eee5f0138a3a413f40f7ebf0684
gbatistaa <gbatistadev@gmail.com>
2025-06-12 17:53:18 +0000

    resolvido o bug da alocação de memória do buffer de console e também adição de atualização de eventos internos

--- 5P/ppc/mpi/projects/vectorial_watch.c
+++ 5P/ppc/mpi/projects/vectorial_watch.c
@@ -5,13 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MSG_SIZE 100
-
-typedef struct message {
-  char message_buffer[MSG_SIZE];
-  int *vectorial_watch;
-} VectorialMessage;
-
 enum Actions {
   intn = 0,
   send = 1,
@@ -28,44 +21,21 @@ int processess_config_size =
     sizeof(processess_config) / sizeof(processess_config[0]);
 
 int create_array_string(int *array, int n, char *buffer, int buf_sz) {
-  int buf_i = 0;
-
-  // Verifica se há espaço mínimo para "{}\0"
-  if (buf_sz < 3) {
-    if (buf_sz > 0)
-      buffer[0] = '\0';
-    return -1; // erro: espaço insuficiente
-  }
-
-  buffer[buf_i++] = '{';
-
   for (int i = 0; i < n; i++) {
-    // Adiciona separador ", " se não for o primeiro número
-    if (i > 0) {
-      if (buf_i + 2 >= buf_sz)
-        goto truncate;
-      buffer[buf_i++] = ',';
-      buffer[buf_i++] = ' ';
-    }
 
-    // Escreve número no buffer
-    int written = snprintf(&buffer[buf_i], buf_sz - buf_i, "%d", array[i]);
-    if (written < 0 || buf_i + written >= buf_sz)
-      goto truncate;
-    buf_i += written;
+    char temp[12];
+
+    sprintf(temp, "%d", array[i]);
+    strcat(buffer, temp);
+
+    if (i < n - 1) {
+      strcat(buffer, ".");
+    }
   }
 
-  if (buf_i + 2 >= buf_sz)
-    goto truncate;
-  buffer[buf_i++] = '}';
-  buffer[buf_i] = '\0';
-  return buf_i; // sucesso: retorna número de caracteres escritos
+  buffer[buf_sz - 1] = '\0';
 
-truncate:
-  // Se buffer for pequeno, termina corretamente com '\0'
-  if (buf_sz > 0)
-    buffer[buf_sz - 1] = '\0';
-  return -1; // erro: buffer truncado
+  return EXIT_SUCCESS;
 }
 
 int count_digits(int number) {
@@ -85,7 +55,7 @@ int count_digits(int number) {
 
 int calculate_buffer_size(int *int_array, int array_length) {
   if (array_length == 0)
-    return 3;
+    return 0;
 
   int total_digit_chars = 0;
 
@@ -93,12 +63,10 @@ int calculate_buffer_size(int *int_array, int array_length) {
     total_digit_chars += count_digits(int_array[i]);
   }
 
-  int comma_spaces = (array_length - 1) * 2;
-  int surrounding_braces = 2;
-  int null_terminator = 1;
+  int total_dots = array_length - 1;
+  int terminator = 1;
 
-  return total_digit_chars + comma_spaces + surrounding_braces +
-         null_terminator;
+  return total_digit_chars + total_dots + terminator;
 }
 
 int main() {
@@ -108,7 +76,11 @@ int main() {
   MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  int *vectorial_watch = calloc(comm_size, sizeof(int));
+  int vectorial_watch[comm_size];
+
+  for (int i = 0; i < comm_size; i++) {
+    vectorial_watch[i] = 0;
+  }
 
   for (int i = 0; i < processess_config_size; i++) {
     bool is_my_turn = processess_config[i][0] == my_rank;
@@ -121,11 +93,14 @@ int main() {
       case intn: {
         // In internal action the process increments 1 in its position on
         // the vectorial watch:
+        vectorial_watch[my_rank]++;
+
         int buffer_size = calculate_buffer_size(vectorial_watch, comm_size);
-        char *buffer = malloc(buffer_size * sizeof(char));
+        char *buffer = calloc(buffer_size, sizeof(char));
+
+        create_array_string(vectorial_watch, comm_size, buffer, buffer_size);
 
         printf("Code %d: Internal Process | %s\n", action, buffer);
-        vectorial_watch[my_rank]++;
         break;
       }
 
@@ -134,7 +109,7 @@ int main() {
                  MPI_COMM_WORLD);
 
         int buffer_size = calculate_buffer_size(vectorial_watch, comm_size);
-        char *buffer = malloc(buffer_size * sizeof(char) + 1);
+        char *buffer = calloc(buffer_size, sizeof(char));
 
         if (buffer == NULL) {
           perror("malloc failed");
@@ -143,28 +118,30 @@ int main() {
 
         create_array_string(vectorial_watch, comm_size, buffer, buffer_size);
 
-        printf("Message sent from %d to %d | %s\n", my_rank, address, buffer);
+        printf("Code %d | Message sent from %d to %d | %s\n", action, my_rank,
+               address, buffer);
         break;
 
       case recv: {
+        vectorial_watch[my_rank]++;
+
         int vectorial_watch_recieved[comm_size];
-
-        int buffer_size =
-            calculate_buffer_size(vectorial_watch_recieved, comm_size);
-        char *buffer = malloc(buffer_size * sizeof(char) + 1);
-
-        if (buffer == NULL) {
-          perror("malloc failed");
-          return 1;
-        }
-
-        create_array_string(vectorial_watch_recieved, comm_size, buffer,
-                            buffer_size);
 
         MPI_Recv(vectorial_watch_recieved, 3, MPI_INT, address, 0,
                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Message recieved by %d from %d | %s\n", my_rank, address,
-               buffer);
+
+        int buffer_size = calculate_buffer_size(vectorial_watch, comm_size);
+        char *buffer = calloc(buffer_size, sizeof(char));
+
+        if (buffer == NULL) {
+          perror("malloc failed");
+          return EXIT_FAILURE;
+        }
+
+        create_array_string(vectorial_watch, comm_size, buffer, buffer_size);
+
+        printf("Code %d | Message recieved by %d from %d | %s\n", action,
+               my_rank, address, buffer);
         break;
       }
       }
