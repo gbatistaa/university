5734eeeaa7206647fce7f3eb8d4b0d4954a2bbd5
Gabriel Barbosa <gbatistadev@gmail.com>
2025-07-08 08:46:34 -0300

    delete makefiles

--- build/CMakeFiles/syncronous_database.dir/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/syncronous_database.dir/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/gbatistadev/Documents/codes/university/build/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
