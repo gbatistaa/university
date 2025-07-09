9abb6c534d1b5d020d23bb1f3c9d3e2da7ad4d31
Gabriel Barbosa <gbatistadev@gmail.com>
2025-07-09 20:08:12 -0300

    compartilhando o build do cmake com o tablet

---
+++ build/CMakeFiles/syncronous_database.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u1/datagrama/gabrielbatista_202300027249_datagrama.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u1/poxim_tech/bst.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u1/poxim_tech/bst.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u1/poxim_tech/bst.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u1/poxim_tech/gabrielbatista_202300027249_porto.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u1/poxim_tech/gabrielbatista_202300027249_porto.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u1/poxim_tech/gabrielbatista_202300027249_porto.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u1/poxim_tech/hash-map.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u1/poxim_tech/hash-map.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u1/poxim_tech/hash-map.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u1/quick_sort/gabrielbatista_202300027249_quicksort.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u1/quick_sort/gabrielbatista_202300027249_quicksort.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u1/quick_sort/gabrielbatista_202300027249_quicksort.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u2/compressao/gabrielbatista_202300027249_compressao.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u2/sequenciamento/gabrielbatista_202300027249_sequenciamento.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u2/sequenciamento/gabrielbatista_202300027249_sequenciamento.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u2/sequenciamento/gabrielbatista_202300027249_sequenciamento.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u2/transportadora/f.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u2/transportadora/f.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u2/transportadora/f.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u2/transportadora/gabrielbatista_202300027249_transportadora.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/paa/u3/labirinto/gabrielbatista_202300027249_labirinto.cpp" "CMakeFiles/syncronous_database.dir/4P/paa/u3/labirinto/gabrielbatista_202300027249_labirinto.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/paa/u3/labirinto/gabrielbatista_202300027249_labirinto.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/4P/statistics/u1/data_analitics.cpp" "CMakeFiles/syncronous_database.dir/4P/statistics/u1/data_analitics.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/4P/statistics/u1/data_analitics.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/5P/so/processosEThreads/cpp/1-threads.cpp" "CMakeFiles/syncronous_database.dir/5P/so/processosEThreads/cpp/1-threads.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/5P/so/processosEThreads/cpp/1-threads.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/5P/so/projects/database/data.cpp" "CMakeFiles/syncronous_database.dir/5P/so/projects/database/data.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/5P/so/projects/database/data.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/5P/so/projects/database/syncronous_database.cpp" "CMakeFiles/syncronous_database.dir/5P/so/projects/database/syncronous_database.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/5P/so/projects/database/syncronous_database.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/5P/so/projects/database/teste.cpp" "CMakeFiles/syncronous_database.dir/5P/so/projects/database/teste.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/5P/so/projects/database/teste.cpp.o.d"
+  "/home/gbatistadev/Documents/codes/university/build/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp" "CMakeFiles/syncronous_database.dir/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp.o" "gcc" "CMakeFiles/syncronous_database.dir/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d"
+  "" "syncronous_database" "gcc" "CMakeFiles/syncronous_database.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/syncronous_database.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for syncronous_database.
+# This may be replaced when dependencies are built.
