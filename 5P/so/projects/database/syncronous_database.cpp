ba3387b98f7c4a500eabb4af6c579113926bc0d7
gbatistaa <gbatistadev@gmail.com>
2025-07-10 17:02:36 +0000

    final

--- 5P/so/projects/database/syncronous_database.cpp
+++ 5P/so/projects/database/syncronous_database.cpp
@@ -20,7 +20,7 @@ using enum ThreadType;
 using variant_t = variant<string, bool, int, double>;
 
 const int THREADS_AVAILABLE = thread::hardware_concurrency();
-const int MAX_READERS = THREADS_AVAILABLE - 6;
+const int MAX_READERS = THREADS_AVAILABLE - 5;
 
 mutex console_mutex;          // Controla escritas simultaneas no log
 mutex control_mutex;          // Controla acesso às variáveis de estado
@@ -219,6 +219,13 @@ void acquire_read_lock(int thread_id) {
            << "\033[34m está esperando para leitura (semáforo cheio).\033[0m\n";
     }
     reader_semaphore.acquire(); // Bloqueia até adquirir
+    {
+      lock_guard<mutex> console_lock(console_mutex);
+      cout << "\033[32mThread \033[33m" << thread_id
+           << "\033[32m entrou no semáforo de leitura. Vagas disponíveis: "
+              "\033[33m"
+           << MAX_READERS - active_readers.load() - 1 << "\033[0m\n";
+    }
   }
 
   unique_lock<mutex> lock(control_mutex);
@@ -235,9 +242,10 @@ void acquire_read_lock(int thread_id) {
     }
   }
 
-  // Espera até que:
-  // 1. Não haja escritor ativo
-  reader_cv.wait(lock, []() { return !writer_active.load(); });
+  // Só vai ter leitura se não tiver nenhuma escrita sendo feita e se esse
+  // leitor for o último da fila
+  reader_cv.wait(lock,
+                 [] { return !writer_active.load() && waiting_writers == 0; });
 
   waiting_readers--;
   active_readers++;
@@ -245,6 +253,7 @@ void acquire_read_lock(int thread_id) {
 
 // Função para liberação de leitura
 void release_read_lock(int thread_id) {
+  // Bloqueia acesso as variáveis atômicas de controle:
   unique_lock<mutex> lock(control_mutex);
   active_readers--;
 
@@ -253,18 +262,16 @@ void release_read_lock(int thread_id) {
     writer_cv.notify_one();
   }
 
-  lock.unlock();
   reader_semaphore.release();
 }
 
 // Função para aquisição de escrita
 void acquire_write_lock(int thread_id) {
+  // Bloqueia acesso as variáveis atômicas de controle:
   unique_lock<mutex> lock(control_mutex);
   waiting_writers++;
 
-  // Espera até que:
-  // 1. Não haja leitores ativos
-  // 2. Não haja escritor ativo
+  // Escrita somente será feita quando não tiver leitor ou escritor ativos
   writer_cv.wait(lock, []() {
     return active_readers.load() == 0 && !writer_active.load();
   });
@@ -283,7 +290,7 @@ void release_write_lock(int thread_id) {
   unique_lock<mutex> lock(control_mutex);
   writer_active = false;
 
-  // Prioridade para escritores: se há escritores esperando, acorda um escritor
+  // se há escritores esperando, acorda um escritor
   // Caso contrário, acorda todos os leitores
   if (waiting_writers > 0) {
     writer_cv.notify_one();
@@ -348,39 +355,64 @@ int main() {
   vector<thread> threads;
 
   int i = 1;
-  // Threads de leitura (primeira parte)
-  for (; i <= THREADS_AVAILABLE / 4; i++) {
+
+  // Leitores 1 a 4
+  for (; i <= 4; i++) {
     threads.emplace_back([i]() {
       routine(i, [i]() { database.tables.at("stocks")->find_all(i); }, false);
     });
   }
 
-  // Threads de escrita
-  for (int j = 0; i <= THREADS_AVAILABLE - 2; i++, j++) {
-    threads.emplace_back([i, j]() {
-      routine(
-          i,
-          [i, j]() {
-            for (const auto &new_company : companies_extra.at(j)) {
-              database.tables.at("stocks")->insert(new_company, i);
-            }
-          },
-          true);
-    });
-  }
+  // Escritor 5
+  threads.emplace_back([i]() {
+    int j = 0;
+    routine(
+        i,
+        [i, j]() {
+          for (const auto &new_company : companies_extra.at(j)) {
+            database.tables.at("stocks")->insert(new_company, i);
+          }
+        },
+        true);
+  });
+  i++;
 
-  // Threads de leitura (segunda parte)
-  for (; i <= THREADS_AVAILABLE; i++) {
-    threads.emplace_back([i]() {
-      routine(
-          i,
-          [i]() {
-            database.tables.at("stocks")->find_where("price", is_bigger_than,
-                                                     500, i);
-          },
-          false);
-    });
-  }
+  // Leitor 6
+  threads.emplace_back([i]() {
+    routine(
+        i,
+        [i]() {
+          database.tables.at("stocks")->find_where("price", is_bigger_than, 500,
+                                                   i);
+        },
+        false);
+  });
+  i++;
+
+  // Escritor 7
+  threads.emplace_back([i]() {
+    int j = 1; // segundo grupo de dados
+    routine(
+        i,
+        [i, j]() {
+          for (const auto &new_company : companies_extra.at(j)) {
+            database.tables.at("stocks")->insert(new_company, i);
+          }
+        },
+        true);
+  });
+  i++;
+
+  // Leitor 8
+  threads.emplace_back([i]() {
+    routine(
+        i,
+        [i]() {
+          database.tables.at("stocks")->find_where("price", is_smaller_than,
+                                                   300, i);
+        },
+        false);
+  });
 
   for (auto &t : threads) {
     t.join();
